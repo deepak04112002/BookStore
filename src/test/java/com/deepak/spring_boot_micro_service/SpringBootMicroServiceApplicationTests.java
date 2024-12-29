d21ffcc9d0b2a6a731363feb96830ba84fb26046
Deepak  Kumar Majhi <dmajhi286@gmail.com>
2024-12-29 19:39:36 +0530

    BookStore project Setup

---
+++ src/test/java/com/deepak/spring_boot_micro_service/SpringBootMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.spring_boot_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
